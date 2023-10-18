137f4931ad045b916d5af3326c3e8a2f72dab216
laurenshu <lauren.shu@kinkaid.org>
2023-10-18 12:41:10 -0500

    rollDice function

--- src/Referee.java
+++ src/Referee.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 public class Referee
 {
     private Board myBoard;
@@ -6,6 +7,13 @@ public class Referee
     {
         myBoard = new Board();
         System.out.println(myBoard);
+    }
+
+    public int rollDice() {
+        final String[] roll_dice = {"1", "2", "3", "4", "5", "6"};
+        Random random = new Random();
+        int index = random.nextInt(roll_dice.length);
+        System.out.println(roll_dice[index]);
     }
 
     public void playGame()
