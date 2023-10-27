bbf5d7959b5ffc3923b954d29df4daba92f3518b
laurenshu <lauren.shu@kinkaid.org>
2023-10-27 12:59:41 -0500

    fixing brackets

--- src/Referee.java
+++ src/Referee.java
@@ -1,27 +1,26 @@
 import java.util.Random;
 import java.util.Scanner;
-public class Referee
-{
+public class Referee {
     private Board myBoard;
 
-    public Referee()
-    {
+    public Referee() {
         myBoard = new Board();
         System.out.println(myBoard);
     }
 
-    public Board getMyBoard(){
+    public Board getMyBoard() {
         return myBoard;
     }
 
     public int[] pickNRandom() {
         int[] randomRolls = new int[2];
-        randomRolls[0] = (int)Math.round((Math.random() * 5.5)+1);
-        randomRolls[1] = (int)Math.round((Math.random() * 5.5)+1);
+        randomRolls[0] = (int) Math.round((Math.random() * 5.5) + 1);
+        randomRolls[1] = (int) Math.round((Math.random() * 5.5) + 1);
         return randomRolls;
 
     }
-    public void askForMovements(int[] rolls){
+
+    public void askForMovements(int[] rolls) {
         System.out.println(rolls[0] + ", " + rolls[1]);
         if (rolls[0] == 5) {
             Scanner myScanner = new Scanner(System.in);
@@ -31,10 +30,6 @@ public class Referee
             if (playerDecision.equals("yes")) {
                 myBoard.checkSafePath(5);
                 // move one player from home to board
-
-
-            } else {
-
             }
             if (rolls[0] == rolls[1]) {
                 System.out.println("Do you want to leave home with your other 5?");
@@ -50,9 +45,9 @@ public class Referee
     }
 
 
-    public void playGame()
-    {
+    public void playGame() {
         askForMovements(pickNRandom());
 
     }
 }
+
